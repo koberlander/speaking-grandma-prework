48e60d9e2d2b6a3a02bfb542e956f7f3d186f1f3
ko kristin <koberlander115@gmail.com>
2019-02-06 17:56:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string === "I LOVE YOU GRANDMA"
+  if string === "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN!"
   elsif string === string.downcase
     return "HUH?! SPEAK UP, SONNY"
