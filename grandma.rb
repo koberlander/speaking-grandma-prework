c45c9b3c101afed700c391d26e9979710aa27d2e
ko kristin <koberlander115@gmail.com>
2019-02-06 18:02:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(string)
   if string === "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN!"
-  elsif string === string.downcase
+  elsif string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
   else
    return "NO, NOT SINCE 1938!" 
